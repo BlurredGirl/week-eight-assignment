75d08a9cb29bd3236422b865bad55c69426ea442
BlurredGirl <163426219+BlurredGirl@users.noreply.github.com>
2024-05-10 23:47:01 +0100

    styled broken comment form and added link to nav

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,6 +10,7 @@ export default function RootLayout({ children }) {
             <Link href="/">H O M E</Link>
             <Link href="/about">A B O U T</Link>
             <Link href="/posts">G A M E S</Link>
+            <Link href="/posts/add-comment">C O M M E N T</Link>
             <Link href="/posts/1">I N D I V I D U A L / G A M E</Link>
           </nav>
         </header>
